5eec6ffdca035b6998cb13f35cb3fff49d429c95
Michael Z <mziacm@gmail.com>
2019-10-09 21:38:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,12 @@ def monopoly_with_third_tier
       :key3 => {},
       :key4 => {}
       },
-    :names => {}
+    :names => {
+      :key1 => {},
+      :key2 => {},
+      :key3 => {},
+      :key4 => {}
+    }
   }}
 end
 
