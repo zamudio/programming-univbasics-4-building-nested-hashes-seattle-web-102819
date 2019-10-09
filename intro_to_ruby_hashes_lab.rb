24250372edafc40314445b690f4f3884160515b8
Michael Z <mziacm@gmail.com>
2019-10-09 21:28:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	base_hash = {}
+	base_hash = {:name => "Michael"}
 end
 
 def monopoly_with_second_tier
@@ -9,7 +9,7 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  base_hash = {:name => "Michael"}
+  #base_hash = {:name => "Michael"}
 end
 
 def monopoly_with_third_tier
