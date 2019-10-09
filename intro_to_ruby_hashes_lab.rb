9adc4130d0fcfb233f7d2c4eb05eb1b022d2a952
Michael Z <mziacm@gmail.com>
2019-10-09 21:44:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,7 @@ def monopoly_with_fourth_tier
       :four_pieces_owned => 200
       },
     :names => {
-      :reading_railroad => {:mortgage_value => "$100"},
+      :reading_railroad => {"mortgage_value" => "$100"},
       :pennsylvania_railroad => {},
       :b_and_o_railroad => {},
       :shortline_railroad => {}
