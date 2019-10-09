9f1bb4233a1d28a57b4fdaf9707469527e5a878d
Michael Z <mziacm@gmail.com>
2019-10-09 21:41:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def monopoly_with_third_tier
       },
     :names => {
       :reading_railroad => {},
-      :key2 => {},
+      :pennsylvania => {},
       :key3 => {},
       :key4 => {}
     }
