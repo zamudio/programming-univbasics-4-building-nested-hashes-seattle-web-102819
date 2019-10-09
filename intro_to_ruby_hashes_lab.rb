fc31a011875af76029247b9137e7c476d146fd80
Michael Z <mziacm@gmail.com>
2019-10-09 21:35:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,10 +8,11 @@ end
 
 def monopoly_with_third_tier
   base_hash = {:railroads => {:rent_in_dollars => {
-    :names => {},
+    :key1 => {},
     :key2 => {},
     :key3 => {},
-    :key4 => {}
+    :key4 => {},
+    :names => {}
   }}}
 end
 
